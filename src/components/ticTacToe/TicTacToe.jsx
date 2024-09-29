f7edee1e8d2de38d409f5bcd6f69f642b894b031
Roman Ponomarev <xgerfy@gmail.com>
2024-09-29 19:38:18 +0400

    add ui for game

--- src/components/ticTacToe/TicTacToe.jsx
+++ src/components/ticTacToe/TicTacToe.jsx
@@ -9,7 +9,23 @@ const TicTacToe = () => {
       <h1 className="title">
         Tic Tac Toe Game In <span>React</span>
       </h1>
-      <div className="board"></div>
+      <div className="board">
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+      </div>
       <button className="reset">Reset</button>
     </div>
   );
